51e3f99fe46d609a81cb4ad4f3a1e350dc281282
gildembergleite <gildembergleite@gmail.com>
2024-06-21 15:57:45 -0300

    feat: add server and index file

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import authRoutes from './routes/auth-routes'
+
+const app = express()
+
+app.use(express.json())
+app.use('/api/auth', authRoutes)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
